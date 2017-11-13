92a688790ed2c264fd6b189d5e14b031c8876199
“Pat <pat.mansfield@hotmail.com>
2017-11-13 11:06:07 +0000

    Update

--- reusable_blog/blog/migrations/0003_post_tag.py
+++ reusable_blog/blog/migrations/0003_post_tag.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reusable_blog', '0002_post_views'),
+        ('resuable_blog', '0002_post_views'),
     ]
 
     operations = [
